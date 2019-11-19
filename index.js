50e281274adc3cef1d21a6089d8da414c3d53572
Christos <constantinides.ch@gmail.com>
2019-11-19 11:39:16 +0200

    ready for deployment

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require("./app"); // the actual Express app
+const http = require("http");
+const config = require("./utils/config");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  const PORT = process.env.PORT || 3001;
+  console.log(`Server running on port ${PORT}`);
+});
